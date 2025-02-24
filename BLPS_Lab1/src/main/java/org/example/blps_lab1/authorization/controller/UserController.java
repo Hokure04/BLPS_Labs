f7178d10fa46b6b0c255a7a0e27508ff953ffb21
jaba <368601@edu.itmo.ru>
2025-02-25 01:59:17 +0300

    update security config, but comment all of it

--- BLPS_Lab1/src/main/java/org/example/blps_lab1/authorization/controller/UserController.java
+++ BLPS_Lab1/src/main/java/org/example/blps_lab1/authorization/controller/UserController.java
@@ -4,6 +4,7 @@ package org.example.blps_lab1.authorization.controller;
 import org.example.blps_lab1.authorization.models.ApplicationStatus;
 import org.example.blps_lab1.authorization.service.impl.ApplicationService;
 import org.example.blps_lab1.authorization.service.impl.UserEnrollmentService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -20,18 +21,20 @@ public class UserController {
     private UserEnrollmentService userEnrollmentService;
     private ApplicationService applicationService;
 
-    // curl -X POST "http://localhost:8080/api/v1/user/application/123"
 
     @PostMapping("/application/{courseId}")
     public void createApplication(@PathVariable Long courseId){
         applicationService.save(courseId);
     }
     
-    // curl -X PUT "http://localhost:8080/api/v1/user/application/456/APPROVED" \
-    //  -H "Authorization: Bearer YOUR_ACCESS_TOKEN"
     @PutMapping("/application/{id}/{status}")
     public void updateApplicationStatus(@PathVariable Long id, @PathVariable String status) {
         userEnrollmentService.processEnrolment(id, ApplicationStatus.valueOf(status.toUpperCase().trim()));
+    }
+
+    @GetMapping("/ping")
+    public String ping(){
+        return "Pong";
     }
 
 }
