faa0d0e5fa115b193bb5e988b1b9fb69f102aee6
ASUS <hokure.04@gmail.com>
2025-05-10 10:36:04 +0300

    transactions adde in new course service

--- BLPS_Lab2/src/main/java/org/example/blps_lab1/adapters/course/service/nw/NewCourseServiceImpl.java
+++ BLPS_Lab2/src/main/java/org/example/blps_lab1/adapters/course/service/nw/NewCourseServiceImpl.java
@@ -20,6 +20,9 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import java.util.ArrayList;
@@ -31,7 +34,7 @@ import java.util.stream.Collectors;
 @Service
 @Slf4j
 public class NewCourseServiceImpl implements NewCourseService {
-    private final TransactionTemplate transactionTemplate;
+    private final PlatformTransactionManager transactionManager;
     private final NewCourseRepository newCourseRepository;
     private final ApplicationRepository applicationRepository;
     private final NewModuleRepository newModuleRepository;
@@ -39,7 +42,7 @@ public class NewCourseServiceImpl implements NewCourseService {
     private final UserService userService;
 
     public NewCourseServiceImpl(PlatformTransactionManager transactionManager, NewCourseRepository newCourseRepository, ApplicationRepository applicationRepository, NewModuleRepository newModuleRepository, StudentRepository studentRepository, UserService userService) {
-        this.transactionTemplate = new TransactionTemplate(transactionManager);
+        this.transactionManager = transactionManager;
         this.newCourseRepository = newCourseRepository;
         this.applicationRepository = applicationRepository;
         this.newModuleRepository = newModuleRepository;
@@ -75,12 +78,20 @@ public class NewCourseServiceImpl implements NewCourseService {
 
     @Override
     public void deleteCourse(UUID courseUUID) {
-        transactionTemplate.execute(status -> {
+        DefaultTransactionDefinition definition = new DefaultTransactionDefinition();
+        definition.setName("deleteCourse");
+        definition.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+        TransactionStatus status = transactionManager.getTransaction(definition);
+        try{
             applicationRepository.deleteAllByNewCourse_Uuid(courseUUID);
 
             newCourseRepository.deleteById(courseUUID);
-            return 0;
-        });
+            transactionManager.commit(status);
+//            return 0;
+        }catch (Exception e){
+            transactionManager.rollback(status);
+            throw e;
+        }
     }
 
     @Override
@@ -90,7 +101,11 @@ public class NewCourseServiceImpl implements NewCourseService {
 
     @Override
     public NewCourse updateCourse(UUID courseUUID, NewCourseDto courseDto) {
-        return transactionTemplate.execute(status -> {
+        DefaultTransactionDefinition definition = new DefaultTransactionDefinition();
+        definition.setName("updateTransaction");
+        definition.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+        TransactionStatus status = transactionManager.getTransaction(definition);
+        try{
             validateRequiredFieldsOrThrow(courseDto);
 
             var oldEntity = newCourseRepository.findById(courseUUID).orElseThrow(() -> new NotExistException("Курс с uuid " + courseUUID + " не найден"));
@@ -103,56 +118,93 @@ public class NewCourseServiceImpl implements NewCourseService {
             if (oldEntity.getNewModuleList() != null) {
                 newEntity.getNewModuleList().addAll(oldEntity.getNewModuleList());
             }
+            transactionManager.commit(status);
             return newCourseRepository.save(newEntity);
-        });
+        }catch (Exception e){
+            transactionManager.rollback(status);
+            throw e;
+        }
     }
 
     @Override
     public List<NewCourse> enrollStudent(Long studentID, UUID courseUUID) {
-        return transactionTemplate.execute(status -> {
+        DefaultTransactionDefinition definition = new DefaultTransactionDefinition();
+        definition.setName("enrollStudent");
+        definition.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+        TransactionStatus status = transactionManager.getTransaction(definition);
+        try{
             var studentEntity = studentRepository.findById(studentID).orElseThrow(() -> new NotExistException("Студент с id: " + studentID + " не найден"));
             var courseEntity = newCourseRepository.findById(courseUUID).orElseThrow(() -> new NotExistException("Курс с uuid " + courseUUID + " не найден"));
 
             studentEntity.getCourses().add(courseEntity);
+            transactionManager.commit(status);
             return studentRepository.save(studentEntity).getCourses();
-        });
+        }catch (Exception e){
+            transactionManager.rollback(status);
+            throw e;
+        }
 
     }
 
     @Override
     public NewCourse addAdditionalCourses(UUID courseUUID, UUID additionalCourseUUID) {
-        return transactionTemplate.execute(status -> {
+        DefaultTransactionDefinition definition = new DefaultTransactionDefinition();
+        definition.setName("addAdditionalCourses");
+        definition.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+        TransactionStatus status = transactionManager.getTransaction(definition);
+        try{
             var coreCourse = newCourseRepository.findById(courseUUID).orElseThrow(() -> new NotExistException("Курс с uuid " + courseUUID + " не найден"));
             var additionalCourse = newCourseRepository.findById(additionalCourseUUID).orElseThrow(() -> new NotExistException("Курс с uuid " + additionalCourseUUID + " не найден"));
 
             coreCourse.getAdditionalCourseList().add(additionalCourse);
+            transactionManager.commit(status);
             return newCourseRepository.save(coreCourse);
-        });
+        }catch (Exception e){
+            transactionManager.rollback(status);
+            throw e;
+        }
     }
 
     @Override
     public NewCourse linkModule(UUID courseUUID, UUID moduleUUID) {
-        return transactionTemplate.execute(status -> {
+        DefaultTransactionDefinition definition = new DefaultTransactionDefinition();
+        definition.setName("linkModule");
+        definition.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+        TransactionStatus status = transactionManager.getTransaction(definition);
+        try{
             var courseEntity = newCourseRepository.findById(courseUUID).orElseThrow(() -> new NotExistException("Курса с uuid: " + courseUUID + " не существует"));
             var moduleEntity = newModuleRepository.findById(moduleUUID).orElseThrow(() -> new NotExistException("Модуль с uuid: " + moduleUUID + " не существует"));
 
             courseEntity.getNewModuleList().add(moduleEntity);
+            transactionManager.commit(status);
             return newCourseRepository.save(courseEntity);
-        });
+        }catch (Exception e){
+            transactionManager.rollback(status);
+            throw e;
+        }
     }
 
     @Override
     public Boolean isCourseFinished(UUID courseUUID) {
-        return transactionTemplate.execute(status -> {
+        DefaultTransactionDefinition definition = new DefaultTransactionDefinition();
+        definition.setName("isCourseFinished");
+        definition.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+        TransactionStatus status = transactionManager.getTransaction(definition);
+        try{
             var courseEntity = newCourseRepository.findById(courseUUID).orElseThrow(() -> new NotExistException("Курса с uuid: " + courseUUID + " не существует"));
 
             for (var module : courseEntity.getNewModuleList()) {
                 if (!isModuleFinished(module.getUuid())) {
+                    transactionManager.commit(status);
                     return false;
                 }
             }
+            transactionManager.commit(status);
             return true;
-        });
+        }catch (Exception e){
+            transactionManager.rollback(status);
+            throw e;
+        }
 
     }
 
