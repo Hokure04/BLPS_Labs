2865120256c7c43cc59d146d98e5adff14f8ec5f
Mikhail Niagin <mikhail.niagin@tune-it.ru>
2025-05-24 02:26:25 +0300

    recover saga parts

--- learning-platform/src/main/java/org/example/blps_lab1/adapters/course/service/CertificateManagerImpl.java
+++ learning-platform/src/main/java/org/example/blps_lab1/adapters/course/service/CertificateManagerImpl.java
@@ -3,6 +3,7 @@ package org.example.blps_lab1.adapters.course.service;
 import java.io.File;
 import java.util.UUID;
 
+import org.example.blps_lab1.adapters.saga.CourseCompletedEvent;
 import org.example.blps_lab1.core.domain.auth.UserXml;
 import org.example.blps_lab1.core.domain.course.nw.NewCourse;
 import org.example.blps_lab1.core.exception.course.InvalidFieldException;
@@ -11,6 +12,7 @@ import org.example.blps_lab1.core.ports.course.CertificateManager;
 import org.example.blps_lab1.core.ports.course.nw.NewCourseService;
 import org.example.blps_lab1.core.ports.email.EmailService;
 import org.example.blps_lab1.core.ports.sss.SimpleStorageService;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
 import lombok.extern.slf4j.Slf4j;
@@ -25,52 +27,56 @@ public class CertificateManagerImpl implements CertificateManager {
     private final SimpleStorageService simpleStorageService;
     private final EmailService emailService;
     private final TransactionTemplate transactionTemplate;
+    private final ApplicationEventPublisher publisher;
 
-    public CertificateManagerImpl(CertificateGenerator certificateGenerator, NewCourseService courseService, SimpleStorageService simpleStorageService, EmailService emailService, PlatformTransactionManager transactionManager) {
+    public CertificateManagerImpl(CertificateGenerator certificateGenerator, NewCourseService courseService, SimpleStorageService simpleStorageService, EmailService emailService, PlatformTransactionManager transactionManager, ApplicationEventPublisher publisher) {
         this.certificateGenerator = certificateGenerator;
         this.courseService = courseService;
         this.simpleStorageService = simpleStorageService;
         this.emailService = emailService;
         this.transactionTemplate = new TransactionTemplate(transactionManager);
+        this.publisher = publisher;
     }
 
     @Override
     public void getCertificate(UserXml user, UUID courseUUID) {
-        transactionTemplate.execute(status -> {
-
-            var course = courseService.getCourseByUUID(courseUUID);
-
-            boolean allModulesCompleted = courseService.isCourseFinished(courseUUID);
-
-            if (!allModulesCompleted) {
-                throw new InvalidFieldException("Курс не пройден до конца");
-            }
-
-            try {
-                var certificatePdf = certificateGenerator.generateCertificate(course.getName(), user.getUsername(), null);
-                saveToSimpleStorageService(user, course, certificatePdf);
-                emailService.sendCertificateToUser(user.getUsername(), certificatePdf);
-            } catch (Exception e) {
-                log.error("Error while creating the certificate", e);
-                sendAboutException(user.getUsername());
-            }
-
-            return 0;
-        });
+        publisher.publishEvent(new CourseCompletedEvent(2L, UUID.randomUUID()));
     }
-
-    private void saveToSimpleStorageService(UserXml user, NewCourse course, File file) {
-        StringBuilder filename = new StringBuilder();
-        filename.append(user.getUsername()).append(course.getName());
-        try {
-            simpleStorageService.uploadFile(user.getUsername(), filename.toString(), file);
-        } catch (Exception e) {
-            log.error("Error while uploading file to minio");
-        }
-    }
-
-    private void sendAboutException(String email) {
-        emailService.informMinioFailure(email);
-        throw new RuntimeException("Ошибка сервиса при создании отчета");//ошибку эту оставить, необходимо, чтобы оно сработала даже в случае успешного письма пользователю об ошибке
-    }
+//        transactionTemplate.execute(status -> {
+//
+//            var course = courseService.getCourseByUUID(courseUUID);
+//
+//            boolean allModulesCompleted = courseService.isCourseFinished(courseUUID);
+//
+//            if (!allModulesCompleted) {
+//                throw new InvalidFieldException("Курс не пройден до конца");
+//            }
+//
+//            try {
+//                var certificatePdf = certificateGenerator.generateCertificate(course.getName(), user.getUsername(), null);
+//                saveToSimpleStorageService(user, course, certificatePdf);
+//                emailService.sendCertificateToUser(user.getUsername(), certificatePdf);
+//            } catch (Exception e) {
+//                log.error("Error while creating the certificate", e);
+//                sendAboutException(user.getUsername());
+//            }
+//
+//            return 0;
+//        });
+//    }
+//
+//    private void saveToSimpleStorageService(UserXml user, NewCourse course, File file) {
+//        StringBuilder filename = new StringBuilder();
+//        filename.append(user.getUsername()).append(course.getName());
+//        try {
+//            simpleStorageService.uploadFile(user.getUsername(), filename.toString(), file);
+//        } catch (Exception e) {
+//            log.error("Error while uploading file to minio");
+//        }
+//    }
+//
+//    private void sendAboutException(String email) {
+//        emailService.informMinioFailure(email);
+//        throw new RuntimeException("Ошибка сервиса при создании отчета");//ошибку эту оставить, необходимо, чтобы оно сработала даже в случае успешного письма пользователю об ошибке
+//    }
 }
