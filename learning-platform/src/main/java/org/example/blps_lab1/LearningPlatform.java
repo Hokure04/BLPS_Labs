ee10c98015080a42271ad067e876c8b7447662f6
Mikhail Niagin <mikhail.niagin@tune-it.ru>
2025-05-23 17:23:13 +0300

    update main class according to article: https://www.mastertheboss.com/jboss-frameworks/spring/spring-boot-hello-world-on-wildfly/?utm_source=chatgpt.com

--- learning-platform/src/main/java/org/example/blps_lab1/LearningPlatform.java
+++ learning-platform/src/main/java/org/example/blps_lab1/LearningPlatform.java
@@ -2,17 +2,25 @@ package org.example.blps_lab1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @EnableTransactionManagement
 @SpringBootApplication
 @EnableWebMvc
-public class LearningPlatform {
+public class LearningPlatform extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearningPlatform.class, args);
 	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(LearningPlatform.class);
+	}
 
+	private static Class<LearningPlatform> applicationClass = LearningPlatform.class;
 }
 
