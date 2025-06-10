cbf690aa27522e003d6b57a6d546272f23959f8a
Mikhail Niagin <mikhail.niagin@tune-it.ru>
2025-06-11 00:52:10 +0300

    add exercise delegate

---
+++ learning-platform/src/main/java/org/example/blps_lab1/adapters/camunda/lms/GetExerciseDelegate.java
@@ -0,0 +1,35 @@
+package org.example.blps_lab1.adapters.camunda.lms;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.camunda.bpm.engine.delegate.BpmnError;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.example.blps_lab1.adapters.camunda.util.CamundaUtils;
+import org.example.blps_lab1.adapters.camunda.util.Codes;
+import org.example.blps_lab1.core.ports.course.nw.NewModuleService;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service("getExercise")
+@AllArgsConstructor
+@Slf4j
+public class GetExerciseDelegate implements JavaDelegate {
+
+    private final NewModuleService newModuleService;
+
+    @Override
+    public void execute(DelegateExecution execution) throws Exception {
+        String chosenModule = CamundaUtils.getVariableString(execution, "chosenModule");
+
+        if (chosenModule == null || chosenModule.isEmpty()) {
+            log.warn("user didn't specify chosenModule field");
+            throw new BpmnError(Codes.ERROR_MESSAGE.getStringName(), "Модуль не выбран");
+        }
+        var module = newModuleService.getModuleByUUID(UUID.fromString(chosenModule));
+        log.info("Found new module {}", module);
+
+        execution.setVariable("exerciseList", module.getExercises().toString());
+    }
+}
